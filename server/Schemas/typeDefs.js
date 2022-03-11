6baab88d488421247d6405f25cd30ba7cd67e99a
GitAndrewCoffee <andrew.mcneal.us@gmail.com>
2022-03-10 20:18:36 -0600

    started mutation

--- server/Schemas/typeDefs.js
+++ server/Schemas/typeDefs.js
@@ -34,11 +34,8 @@ const typeDefs = gql`
     addUser(username: String!, email: String!, password: String!): Auth
     saveBook(authors: [String!], description: String!, title: String!, bookId: String!, image: String!, link: String!): User
     removeBook(bookId: String!): User
-  }
-  
+  }  
 `;
-
-
 
 // export the typeDefs
 module.exports = typeDefs;
